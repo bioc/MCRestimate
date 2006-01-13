1faa79aa8306c5f2c6d13c99bf5cdcafdced7b4f
Markus Ruschhaupt <m.ruschhaupt@dkfz-heidelberg.de>
2006-01-13 15:32:25 +0000

    tune update - eset removed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MCRestimate@15704 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ClassifierBuild.R
+++ R/ClassifierBuild.R
@@ -56,120 +56,6 @@ ClassifierBuild.default <- function(eset,
 
 
 
-Mytune<-function (method, train.x, train.y = NULL, data = list(), validation.x = NULL,
-    validation.y = NULL, ranges = NULL, predict.func = predict,
-    tunecontrol = tune.control(), ...)
-{
-    call <- match.call()
-    resp <- function(formula, data) model.response(model.frame(formula,
-        data))
-    classAgreement <- function(tab) {
-        n <- sum(tab)
-        if (!is.null(dimnames(tab))) {
-            lev <- intersect(colnames(tab), rownames(tab))
-            p0 <- sum(diag(tab[lev, lev]))/n
-        }
-        else {
-            m <- min(dim(tab))
-            p0 <- sum(diag(tab[1:m, 1:m]))/n
-        }
-        p0
-    }
-    if (!is.character(method))
-        method2 <- deparse(substitute(method))
-    else
-        method2 <- method
-    if (tunecontrol$sampling == "cross")
-        validation.x <- validation.y <- NULL
-    useFormula <- is.null(train.y)
-    if (useFormula && (is.null(data) || length(data) == 0))
-        data <- model.frame(train.x)
-    if (is.vector(train.x))
-        train.x <- t(t(train.x))
-    if (is.data.frame(train.y))
-        train.y <- as.matrix(train.y)
-    if (!is.null(validation.x))
-        tunecontrol$fix <- 1
-    n <- nrow(if (useFormula)
-        data
-    else train.x)
-    perm.ind <- sample(n)
-    if (tunecontrol$sampling == "cross") {
-        if (tunecontrol$cross > n)
-            stop("`cross' must not exceed sampling size!")
-        if (tunecontrol$cross == 1)
-            stop("`cross' must be greater than 1!")
-    }
-    train.ind <- if (tunecontrol$sampling == "cross")
-        tapply(1:n, cut(1:n, breaks = tunecontrol$cross), function(x) perm.ind[-x])
-    else if (tunecontrol$sampling == "fix")
-        list(perm.ind[1:trunc(n * tunecontrol$fix)])
-    else lapply(1:tunecontrol$nboot, function(x) sample(n, n *
-        tunecontrol$boot.size))
-    parameters <- if (is.null(ranges))
-        data.frame(dummyparameter = 0)
-    else expand.grid(ranges)
-    p <- nrow(parameters)
-    if (!is.logical(tunecontrol$random)) {
-        if (tunecontrol$random < 1)
-            stop("random must be a strictly positive integer")
-        if (tunecontrol$random > p)
-            tunecontrol$random <- p
-        parameters <- parameters[sample(1:p, tunecontrol$random),
-            ]
-    }
-    model.errors <- c()
-    for (para.set in 1:p) {
-        sampling.errors <- c()
-        for (sample in 1:length(train.ind)) {
-            repeat.errors <- c()
-            for (reps in 1:tunecontrol$nrepeat) {
-                pars <- if (is.null(ranges))
-                  NULL
-                else lapply(parameters[para.set, , drop = FALSE],
-                  unlist)
-                model <- if (useFormula)
-                  do.call(method, c(list(train.x, data = data,subset = train.ind[[sample]]), pars, list(...)))
-                else do.call(method, c(list(train.x[train.ind[[sample]],], y = train.y[train.ind[[sample]]]), pars,list(...)))
-                pred <- predict.func(model, if (!is.null(validation.x))
-                  validation.x
-                else if (useFormula)
-                  data[-train.ind[[sample]], , drop = FALSE]
-                else train.x[-train.ind[[sample]], , drop = FALSE])
-                true.y <- if (!is.null(validation.y))
-                  validation.y
-                else if (useFormula)
-                  resp(train.x, data[-train.ind[[sample]], ])
-                else train.y[-train.ind[[sample]]]
-                repeat.errors[reps] <- if (is.factor(true.y))
-                  1 - classAgreement(table(pred, true.y))
-                else crossprod(pred - true.y)/length(pred)
-            }
-            sampling.errors[sample] <- tunecontrol$repeat.aggregate(repeat.errors)
-        }
-        model.errors[para.set] <- tunecontrol$sampling.aggregate(sampling.errors)
-    }
-    best <- which.min(model.errors)
-    pars <- if (is.null(ranges))
-        NULL
-    else lapply(parameters[best, , drop = FALSE], unlist)
-    structure(list(best.parameters = parameters[best, , drop = FALSE],
-        best.performance = model.errors[best], method = method2,
-        nparcomb = nrow(parameters), train.ind = train.ind, sampling = switch(tunecontrol$sampling,
-            fix = "fixed training/validation set", bootstrap = "bootstrapping",
-            cross = if (tunecontrol$cross == n) "leave-one-out" else paste(tunecontrol$cross,
-                "-fold cross validation", sep = "")), performances = if (tunecontrol$performances) cbind(parameters,
-            error = model.errors), best.model = if (tunecontrol$best.model) {
-            modeltmp <- if (useFormula) do.call(method, c(list(train.x,
-                data = data), pars, list(...))) else do.call(method,
-                c(list(x = train.x, y = train.y), pars, list(...)))
-            call[[1]] <- as.symbol("best.tune")
-            modeltmp$call <- call
-            modeltmp
-        }), class = "tune")
-}
-
-  
   
   preprocessing <- function(x,label,PreprocessingSlots=tIp,...)
    { m <- t(x)
@@ -218,7 +104,7 @@ Mytune<-function (method, train.x, train.y = NULL, data = list(), validation.x =
 
 # now the 'best' parameter are calculated if this is nessessary ( if there are choices)
      if (! (all(sapply(poss.parameters,length) %in% 1))) {
-    result <- Mytune(the.function.for.classification,
+    result <- tune(the.function.for.classification,
 	           train.matrix,
 	           train.factor,
 	           ranges=poss.parameters,
@@ -318,122 +204,6 @@ ClassifierBuild.exprSetRG<- function(eset,
 
 
 
-Mytune<-function (method, train.x, train.y = NULL, data = list(), validation.x = NULL,
-    validation.y = NULL, ranges = NULL, predict.func = predict,
-    tunecontrol = tune.control(), ...)
-{
-    call <- match.call()
-    resp <- function(formula, data) model.response(model.frame(formula,
-        data))
-    classAgreement <- function(tab) {
-        n <- sum(tab)
-        if (!is.null(dimnames(tab))) {
-            lev <- intersect(colnames(tab), rownames(tab))
-            p0 <- sum(diag(tab[lev, lev]))/n
-        }
-        else {
-            m <- min(dim(tab))
-            p0 <- sum(diag(tab[1:m, 1:m]))/n
-        }
-        p0
-    }
-    if (!is.character(method))
-        method2 <- deparse(substitute(method))
-    else
-        method2 <- method
-    if (tunecontrol$sampling == "cross")
-        validation.x <- validation.y <- NULL
-    useFormula <- is.null(train.y)
-    if (useFormula && (is.null(data) || length(data) == 0))
-        data <- model.frame(train.x)
-    if (is.vector(train.x))
-        train.x <- t(t(train.x))
-    if (is.data.frame(train.y))
-        train.y <- as.matrix(train.y)
-    if (!is.null(validation.x))
-        tunecontrol$fix <- 1
-    n <- nrow(if (useFormula)
-        data
-    else train.x)
-    perm.ind <- sample(n)
-    if (tunecontrol$sampling == "cross") {
-        if (tunecontrol$cross > n)
-            stop("`cross' must not exceed sampling size!")
-        if (tunecontrol$cross == 1)
-            stop("`cross' must be greater than 1!")
-    }
-    train.ind <- if (tunecontrol$sampling == "cross")
-        tapply(1:n, cut(1:n, breaks = tunecontrol$cross), function(x) perm.ind[-x])
-    else if (tunecontrol$sampling == "fix")
-        list(perm.ind[1:trunc(n * tunecontrol$fix)])
-    else lapply(1:tunecontrol$nboot, function(x) sample(n, n *
-        tunecontrol$boot.size))
-    parameters <- if (is.null(ranges))
-        data.frame(dummyparameter = 0)
-    else expand.grid(ranges)
-    p <- nrow(parameters)
-    if (!is.logical(tunecontrol$random)) {
-        if (tunecontrol$random < 1)
-            stop("random must be a strictly positive integer")
-        if (tunecontrol$random > p)
-            tunecontrol$random <- p
-        parameters <- parameters[sample(1:p, tunecontrol$random),
-            ]
-    }
-    model.errors <- c()
-    for (para.set in 1:p) {
-        sampling.errors <- c()
-        for (sample in 1:length(train.ind)) {
-            repeat.errors <- c()
-            for (reps in 1:tunecontrol$nrepeat) {
-                pars <- if (is.null(ranges))
-                  NULL
-                else lapply(parameters[para.set, , drop = FALSE],
-                  unlist)
-                model <- if (useFormula)
-                  do.call(method, c(list(train.x, data = data,subset = train.ind[[sample]]), pars, list(...)))
-                else do.call(method, c(list(train.x[train.ind[[sample]],], y = train.y[train.ind[[sample]]]), pars,list(...)))
-                pred <- predict.func(model, if (!is.null(validation.x))
-                  validation.x
-                else if (useFormula)
-                  data[-train.ind[[sample]], , drop = FALSE]
-                else train.x[-train.ind[[sample]], , drop = FALSE])
-                true.y <- if (!is.null(validation.y))
-                  validation.y
-                else if (useFormula)
-                  resp(train.x, data[-train.ind[[sample]], ])
-                else train.y[-train.ind[[sample]]]
-                repeat.errors[reps] <- if (is.factor(true.y))
-                  1 - classAgreement(table(pred, true.y))
-                else crossprod(pred - true.y)/length(pred)
-            }
-            sampling.errors[sample] <- tunecontrol$repeat.aggregate(repeat.errors)
-        }
-        model.errors[para.set] <- tunecontrol$sampling.aggregate(sampling.errors)
-    }
-    best <- which.min(model.errors)
-    pars <- if (is.null(ranges))
-        NULL
-    else lapply(parameters[best, , drop = FALSE], unlist)
-    structure(list(best.parameters = parameters[best, , drop = FALSE],
-        best.performance = model.errors[best], method = method2,
-        nparcomb = nrow(parameters), train.ind = train.ind, sampling = switch(tunecontrol$sampling,
-            fix = "fixed training/validation set", bootstrap = "bootstrapping",
-            cross = if (tunecontrol$cross == n) "leave-one-out" else paste(tunecontrol$cross,
-                "-fold cross validation", sep = "")), performances = if (tunecontrol$performances) cbind(parameters,
-            error = model.errors), best.model = if (tunecontrol$best.model) {
-            modeltmp <- if (useFormula) do.call(method, c(list(train.x,
-                data = data), pars, list(...))) else do.call(method,
-                c(list(x = train.x, y = train.y), pars, list(...)))
-            call[[1]] <- as.symbol("best.tune")
-            modeltmp$call <- call
-            modeltmp
-        }), class = "tune")
-}
-
-
-  
-
   preprocessing <- function(x,label,PreprocessingSlots=tIp,...)
    { m <- t(x)
      for (i in 1:length(thePreprocessingMethods))
@@ -487,7 +257,7 @@ Mytune<-function (method, train.x, train.y = NULL, data = list(), validation.x =
  train.factor <- class.column.factor
 
   if (! (all(sapply(poss.parameters,length) %in% 1))){
-     result<-Mytune(the.function.for.classification,
+     result<-tune(the.function.for.classification,
 	          train.matrix,
 	          train.factor,
 	          ranges=poss.parameters,
