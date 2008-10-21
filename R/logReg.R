38da164c478ec39094a74c36692b4c5fc8850fd3
Marc Johannes <m.johannes@dkfz.de>
2008-10-21 15:23:00 +0000

    updated documentation added NAMESPACES
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MCRestimate@34787 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/logReg.R
+++ R/logReg.R
@@ -5,37 +5,40 @@
 
 
 PLR<- function (trainmatrix, resultvector, kappa = 0, eps = 1e-04)
-{   if (!length(resultvector) == nrow(trainmatrix))
-        stop("The data matrix must have as many rows as the class label vector.")
-    if (!length(unique(resultvector)) == 2)
-        stop("PLR only works with exactly two different classes")
-    class.factor <- as.factor(resultvector)
-    factorlevel <- list(level0 = c(), level1 = c())
-    factorlevel$level0 <- levels(class.factor)[1]
-    factorlevel$level1 <- levels(class.factor)[2]
-    resultvector <- as.numeric(class.factor) - 1
-    m <- nrow(trainmatrix)
-    n <- ncol(trainmatrix)
-    trainmatrix.c <- apply(trainmatrix, 2, function(k) k - mean(k))
-    s <- svd(trainmatrix.c, nu = m)
-    R <- s$u * matrix(s$d, m, m, byrow = TRUE)
-    ty <- rep(1, m)
-    aics <- trs <- double()
-    results <- list()
-    for (i in 1:length(kappa)){
-      kapp <- kappa[i]
-      FIT     <- plr.fit(resultvector, ty, R, kapp, eps = eps)
-      results[[i]] <- FIT
-      print(c(log10(kapp), FIT$aic, FIT$tr))
-    }
-    aics <- sapply(results,function(x) x$aic)
-    trs <- sapply(results,function(x) x$tr)
-    aicbest <- which.min(aics)
-    res <- results[[aicbest]]
-    b <- s$v %*% res$a[-1]
-    result <- list(a = res$a[1], b = b, factorlevel = factorlevel, aics=aics,trs=trs,kappas=kappa)
-    class(result) <- "PLR"
-    return(result)
+{
+	if (!length(resultvector) == nrow(trainmatrix))
+		stop("The data matrix must have as many rows as the class label vector.")
+	
+	if (!length(unique(resultvector)) == 2)
+		stop("PLR only works with exactly two different classes")
+	
+	class.factor <- as.factor(resultvector)
+	factorlevel <- list(level0 = c(), level1 = c())
+	factorlevel$level0 <- levels(class.factor)[1]
+	factorlevel$level1 <- levels(class.factor)[2]
+	resultvector <- as.numeric(class.factor) - 1
+	m <- nrow(trainmatrix)
+	n <- ncol(trainmatrix)
+	trainmatrix.c <- apply(trainmatrix, 2, function(k) k - mean(k))
+	s <- svd(trainmatrix.c, nu = m)
+	R <- s$u * matrix(s$d, m, m, byrow = TRUE)
+	ty <- rep(1, m)
+	aics <- trs <- double()
+	results <- list()
+	for (i in 1:length(kappa)){
+		kapp <- kappa[i]
+		FIT     <- plr.fit(resultvector, ty, R, kapp, eps = eps)
+		results[[i]] <- FIT
+		print(c(log10(kapp), FIT$aic, FIT$tr))
+	}
+	aics <- sapply(results,function(x) x$aic)
+	trs <- sapply(results,function(x) x$tr)
+	aicbest <- which.min(aics)
+	res <- results[[aicbest]]
+	b <- s$v %*% res$a[-1]
+	result <- list(a = res$a[1], b = b, factorlevel = factorlevel, aics=aics,trs=trs,kappas=kappa)
+	class(result) <- "PLR"
+	return(result)
 }
 
 
@@ -55,44 +58,46 @@ predict.PLR <- function(object,...)
 
  plr.fit <- function(y, ty, X0, kappa=0, eps=1e-4)  
  {
-  m <- nrow(X0)
-  X0 <- apply(X0, 2, function(x) x-mean(x))
-  X <- cbind(rep(1,m), X0)
-  n <- ncol(X)
- 
- #
-  p0 <- (sum(y)+1)/(sum(ty)+2)
-  a <- rep(0, n)
-  a[1] <- log(p0/(1-p0))
-  R <- diag(x=kappa, nrow=n)
-  R[1,1] <- 0
-
- #
- repeatit <- 1
- it <- 0
- while(repeatit) {
-    it <- it + 1
-    z <- X %*% a
-    a1 <- a
-    p <- 1/(1+exp(-z))
-    mu <- ty*p
-    w <- mu * (1-p)
-    W <- matrix(w, m, n)
-    G0 <- t(X) %*% (W*X)
-    G <- G0 + R
-    a <- solve(G, t(X) %*% (y-mu+w*z))
-    da <- max(abs(a-a1))
-    repeatit <- (da / max(abs(a))) > eps
- }
- dev <- bindev(y, ty, p)
- tr <- sum(diag(solve(G)%*%G0))
- aic <- dev + 2 * tr
- return(list(p=p, a=a, aic=aic, tr=tr, z=z))
+	  m <- nrow(X0)
+	  X0 <- apply(X0, 2, function(x) x-mean(x))
+	  X <- cbind(rep(1,m), X0)
+	  n <- ncol(X)
+	 
+	 #
+	  p0 <- (sum(y)+1)/(sum(ty)+2)
+	  a <- rep(0, n)
+	  a[1] <- log(p0/(1-p0))
+	  R <- diag(x=kappa, nrow=n)
+	  R[1,1] <- 0
+	
+	 #
+	 repeatit <- 1
+	 it <- 0
+	 while(repeatit)
+	 {
+	    it <- it + 1
+	    z <- X %*% a
+	    a1 <- a
+	    p <- 1/(1+exp(-z))
+	    mu <- ty*p
+	    w <- mu * (1-p)
+	    W <- matrix(w, m, n)
+	    G0 <- t(X) %*% (W*X)
+	    G <- G0 + R
+	    a <- solve(G, t(X) %*% (y-mu+w*z))
+	    da <- max(abs(a-a1))
+	    repeatit <- (da / max(abs(a))) > eps
+	 }
+	 dev <- bindev(y, ty, p)
+	 tr <- sum(diag(solve(G)%*%G0))
+	 aic <- dev + 2 * tr
+	 return(list(p=p, a=a, aic=aic, tr=tr, z=z))
  }
 
 
  bindev <- function(y, ty, p)
  {
+	 
    mu <- ty * p
    r <- ty - y
    f1 <- (y > 0)
@@ -100,7 +105,7 @@ predict.PLR <- function(object,...)
    d1 <- sum(y[f1] * log(y[f1] / mu[f1]))
    d2 <- sum(r[f2] * log(r[f2] / (ty[f2] - mu[f2])))
    d = 2 * d1 + 2 * d2;
- return(d)
+	 return(d)
  }
 
 
